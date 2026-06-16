7284f302eb7fa182962178d87b9c4867307b7703
fxbenard <fx@wp-translations.pro>
2026-06-17 00:42:38 +0200

    fix: footer GitHub link to JustPack/justpack

--- client/components/blackout-footer.vue
+++ client/components/blackout-footer.vue
@@ -8,7 +8,7 @@
             JustPack is an independent open-source packing tool built on <a class="lpHref" href="https://lighterpack.com" target="_blank" rel="noopener noreferrer">LighterPack</a>.
         </div>
         <div class="lpContact">
-            <a class="lpHref" href="https://github.com/fxbenard/lighterpack" target="_blank" rel="noopener noreferrer">Open source</a>
+            <a class="lpHref" href="https://github.com/JustPack/justpack" target="_blank" rel="noopener noreferrer">Open source</a>
             -
             <a class="lpHref" href="mailto:info@lighterpack.app">Contact</a>
         </div>
